891dd526912bbcd98e6326fb800fbad10d9ee24a
UrmatChalbaev932 <157380726+UrmatChalbaev932@users.noreply.github.com>
2024-07-01 13:39:57 +0500

    Update ChromeWebDriver.java

--- src/main/java/org/example/ui/drivers/ChromeWebDriver.java
+++ src/main/java/org/example/ui/drivers/ChromeWebDriver.java
@@ -28,3 +28,54 @@ public class ChromeWebDriver {
         return driver;
     }
 }
+/*public class ConfigReader {
+
+Объявляет публичный класс ConfigReader, который будет использоваться для чтения конфигурационных данных.
+private static Properties properties;
+
+Объявляет приватное статическое поле properties, которое будет использоваться для хранения загруженных свойств из файла.
+private ConfigReader() {
+
+Объявляет приватный конструктор класса ConfigReader, чтобы предотвратить создание экземпляров этого класса извне.
+static {
+
+Начало статического блока инициализации. Этот блок будет выполнен один раз при первой загрузке класса.
+try {
+
+Начало блока try, в котором выполняются операции, которые могут вызвать исключения.
+String path = "src/main/resources/application.properties";
+
+Определение пути к файлу application.properties, который содержит конфигурационные данные.
+FileInputStream inputStream = new FileInputStream(path);
+
+Создание потока inputStream для чтения данных из файла, указанного в переменной path.
+properties = new Properties();
+
+Создание нового объекта Properties, который будет использоваться для хранения свойств из файла.
+properties.load(inputStream);
+
+Загрузка свойств из потока inputStream в объект properties.
+inputStream.close();
+
+Закрытие потока inputStream после завершения чтения данных из файла.
+} catch (IOException e) {
+
+Начало блока catch, который обрабатывает исключения типа IOException, возникающие при ошибках ввода-вывода.
+throw new RuntimeException("file not found");
+
+В случае возникновения исключения IOException, выбрасывается новое исключение типа RuntimeException с сообщением "file not found".
+}
+
+Завершение блока try-catch.
+public static String getValue(String key){
+
+Определение публичного статического метода getValue, который принимает ключ key и возвращает значение свойства из объекта properties.
+return properties.getProperty(key).trim();
+
+Возвращает значение свойства, связанное с указанным ключом key. Метод trim() используется для удаления начальных и конечных пробелов из строки значения.
+}
+
+Завершение метода getValue.
+}
+
+Завершение класса ConfigReader. */
